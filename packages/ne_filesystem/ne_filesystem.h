d71628e0105eed4205650597367f5f198b5aea81
TrevorSundberg <Trevor.Sundberg@gmail.com>
2018-07-28 23:47:20 -0700

    Added proper dependencies on sub-apis to the corresponding test packages. Updated filesystem comments and names.

--- packages/ne_filesystem/ne_filesystem.h
+++ packages/ne_filesystem/ne_filesystem.h
@@ -6,8 +6,8 @@ NE_BEGIN
 NE_DECLARE_PACKAGE(ne_filesystem, 0, 0);
 
 // All file system paths start with '/' and use '/' as a separator between all directories.
-// This way is chosen to keep code platform agnostic. To translate between the current operating system use
-// the translate functions.
+// This way is chosen to keep code platform agnostic. To translate between the current
+// operating system format use the translate functions.
 
 // We need to extend code to deal with symbolic links and permissions.
 
@@ -17,10 +17,13 @@ static const ne_filesystem_mode ne_filesystem_mode_write = 1;
 static const ne_filesystem_mode ne_filesystem_mode_append = 2;
 static const ne_filesystem_mode ne_filesystem_mode_count = 3;
 
+// This function should only be used for displaying a string to the user.
 NE_API const char* (*ne_filesystem_translate_os_to_ne)(const char* path);
+
+// This function should only be used for parsing a file path provided by the user.
 NE_API const char* (*ne_filesystem_translate_ne_to_os)(const char* path);
 
-NE_API void (*ne_filesystem_open_file)(uint64_t* result, ne_filesystem_mode mode, const char* filename, ne_core_stream* stream_out);
+NE_API void (*ne_filesystem_open_file)(uint64_t* result, ne_filesystem_mode mode, const char* path, ne_core_stream* stream_out);
 
 NE_API const char* (*ne_filesystem_get_path_separator)(uint64_t* result);
 
@@ -28,7 +31,7 @@ NE_API const char* (*ne_filesystem_get_path_separator)(uint64_t* result);
 NE_API const char* (*ne_filesystem_get_working_directory)(uint64_t* result);
 
 // Set the current working directory (all relative paths are relative to this location).
-NE_API const char* (*ne_filesystem_set_working_directory)(uint64_t* result, const char* directory);
+NE_API const char* (*ne_filesystem_set_working_directory)(uint64_t* result, const char* path);
 
 // Get the directory that the executable lives in.
 NE_API const char* (*ne_filesystem_get_executable_directory)(uint64_t* result);
@@ -56,7 +59,7 @@ struct ne_filesystem_dialog_config
   const char* filter;
   
   // .
-  const char* starting_file_name;
+  const char* starting_path;
   ne_core_bool multi_select;
   ne_filesystem_dialog_mode mode;
   ne_filesystem_dialog_type type;
