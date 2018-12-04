64f7800aa6f7798ddadd5d82d2c59c951fe74a2e
TrevorSundberg <Trevor.Sundberg@gmail.com>
2018-12-04 02:11:55 -0800

    Now running msbuild on win32. Cleanup in build.js.

--- build.js
+++ build.js
@@ -2,30 +2,58 @@ const execa = require('execa');
 const path = require('path');
 const mkdirp = require('mkdirp');
 const fs = require('fs');
-//const file = require('file'); // Remove me if unused
 const glob = require('glob');
+const os = require('os');
 
-async function main()
+function gatherDirectories()
 {
   const rootDir = process.cwd();
+  return {
+    root: rootDir,
+    packages: path.join(rootDir, 'packages'),
+    build: path.join(rootDir, 'build'),
+  };
+}
 
-  // Run eslint.
+function safeDeleteFile(filePath)
+{
+  try
+  {
+    fs.unlinkSync(filePath);
+  }
+  catch (err)
+  {
+    // Ignored.
+  }
+}
+
+function gatherSourceFiles(dirs)
+{
+  // Gather all .c, .cpp, and .h files.
+  return glob.sync('**/*.@(c|cpp|h)', {
+    cwd: dirs.packages
+  });
+}
+
+async function runEslint(dirs)
+{
   const eslintOptions = {
-    cwd: rootDir,
-    stdio: ['ignore', 'ignore', 'inherit'],
+    cwd: dirs.root,
+    stdio: ['ignore', 'pipe', 'inherit'],
     reject: false
   };
-  await execa('node', ['node_modules/eslint/bin/eslint.js', '.'], eslintOptions);
+  const result = await execa('node', ['node_modules/eslint/bin/eslint.js', '.'], eslintOptions);
+  if (result.stdout)
+  {
+    console.error(result.stdout);
+  }
+}
 
-  // Gather all .c, .cpp, and .h files.
-  const packagesDir = path.join(rootDir, 'packages');
-  const sourceFiles = glob.sync('**/*.@(c|cpp|h)', {
-    cwd: packagesDir
-  });
-
+async function runClangTidy(dirs, sourceFiles)
+{
   // Run clang-tidy.
   const clangTidyOptions = {
-    cwd: packagesDir,
+    cwd: dirs.packages,
     stdio: ['ignore', 'pipe', 'ignore'],
     reject: false
   };
@@ -39,10 +67,12 @@ async function main()
       console.error(result.stdout);
     }
   }
+}
 
-  // Run clang-format.
+async function runClangFormat(dirs, sourceFiles)
+{
   const clangFormatOptions = {
-    cwd: packagesDir,
+    cwd: dirs.packages,
     stdio: ['ignore', 'pipe', 'ignore'],
     reject: false,
     stripFinalNewline: false,
@@ -55,7 +85,7 @@ async function main()
     // did not auto-format their code, so we will perform a diff.
     const result = await execa('clang-format', [filePath], clangFormatOptions);
 
-    const fullPath = path.join(packagesDir, filePath);
+    const fullPath = path.join(dirs.packages, filePath);
     const oldCode = fs.readFileSync(fullPath, {
       encoding: 'utf8'
     });
@@ -66,31 +96,91 @@ async function main()
       console.error(`File '${fullPath}' was not formatted`);
     }
   }
+}
 
-  // Run doxygen.
+async function runDoxygen(dirs)
+{
   const doxygenOptions = {
-    cwd: rootDir,
+    cwd: dirs.root,
     stdio: ['ignore', 'ignore', 'inherit'],
     reject: false
   };
   await execa('doxygen', [], doxygenOptions);
+}
 
-  // Run cmake.
-  const buildDir = path.join(rootDir, 'build');
+async function runCmake(dirs)
+{
   const cmakeOptions = {
-    cwd: buildDir,
+    cwd: dirs.build,
     stdio: ['ignore', 'ignore', 'inherit'],
     reject: false
   };
-  mkdirp.sync(buildDir, {
+  mkdirp.sync(dirs.build, {
     recursive: true
   });
   await execa('cmake', ['..'], cmakeOptions);
+}
 
+async function runBuild(dirs)
+{
+  if (os.platform() === 'win32')
+  {
+    const vswhereOptions = {
+      cwd: `${process.env['ProgramFiles(x86)']}/Microsoft Visual Studio/Installer/`,
+      stdio: ['ignore', 'pipe', 'inherit'],
+      reject: false
+    };
+    const result = await execa('vswhere.exe', vswhereOptions);
+    if (!result.stdout)
+    {
+      console.error('No output from vswhere.exe');
+      return;
+    }
+    const devenvPath = /productPath: (.*)/g.exec(result.stdout)[1];
+    if (!devenvPath)
+    {
+      console.error('No "productPath" from vswhere.exe');
+      return;
+    }
+
+    const logPath = path.join(dirs.build, 'output.log');
+    safeDeleteFile(logPath);
+
+    const devenvOptions = {
+      cwd: dirs.build,
+      stdio: ['ignore', 'ignore', 'inherit'],
+      reject: false
+    };
+    const args = [path.join(dirs.build, 'ne.sln'), '/build', 'Debug', '/out', logPath];
+    await execa(devenvPath, args, devenvOptions);
+    const log = fs.readFileSync(logPath, {
+      encoding: 'utf8'
+    });
+    safeDeleteFile(logPath);
+
+    if (!log.match(/, 0 failed,/g))
+    {
+      console.error(log);
+    }
+  }
+  else
+  {
+    console.error('Unhandled platform');
+  }
+}
+
+async function main()
+{
+  const dirs = gatherDirectories();
+  await runEslint(dirs);
+  const sourceFiles = gatherSourceFiles(dirs);
+  await runClangTidy(dirs, sourceFiles);
+  await runClangFormat(dirs, sourceFiles);
+  await runDoxygen(dirs);
+  // Run moxygen.
+  await runCmake(dirs);
+  await runBuild(dirs);
+  // Run tests.
 }
 
 main();
-
-// Run tests
-// Run doxygen
-// Run moxygen
